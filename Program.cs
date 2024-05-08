0fc1a076bc6e2b7958760a5da86ca728eba62eb3
chrishamillsnyk <88782898+chrishamillsnyk@users.noreply.github.com>
2024-05-08 20:47:59 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,3 +10,11 @@ namespace dotNet_goof
         }
     }
 }
+
+// Checks whether the user has been found with username and password
+public async Task<bool> CheckLogin([FromForm] LoginForm formModel)
+{
+  // Real code should use a hashed and salted password
+  var sql = $"SELECT * FROM users WHERE email={formModel.Email} AND password={formModel.Password}";
+  return await dbCtx.Users.FromSqlRaw(sql).AnyAsync();
+}
